957b1e515216b92906d47c1184b826b412e898d9
Rahil Khan <rashidali.rahil@gmail.com>
2017-01-14 15:02:59 +0530

    addded working rest call

--- SourceCode/ManageExpenses/WebContent/resources/javascript/application/dashboard.js
+++ SourceCode/ManageExpenses/WebContent/resources/javascript/application/dashboard.js
@@ -18,7 +18,7 @@ if (typeof me.application.dashboard == "undefined") {
 			onload : function(){
 				console.log("dashboard.js : onload");
 				me.application.dashboard.getDashboardDetails();
-
+				me.application.dashboard.getRequirementTemplateData();
 
 			},
 
@@ -32,6 +32,7 @@ if (typeof me.application.dashboard == "undefined") {
 					datatype: "json",
 				}).done(function(result) {
 					result = JSON.parse(result);
+					console.log("\tresult : " + result);
 					if (result.sessionMessage != null && result.sessionMessage != "") {
 						console.log("\t" + result.sessionMessage);
 					} else if (result != null && result.errorMessage != null) {
@@ -43,7 +44,34 @@ if (typeof me.application.dashboard == "undefined") {
 						console.log("getDashboardDetails : \n\tresult : " + result);
 					}
 				}).fail(function(jqXHR, textStatus) {
-					console.log("Application Exception Occured \n\tjqXHR : " + JSON.stringify(jqXHR) + "\n\ttextStatus : " + textStatus);
+					console.log("getDashboardDetails : Application Exception Occured \n\tjqXHR : " + JSON.stringify(jqXHR) + "\n\ttextStatus : " + textStatus);
+				});
+				return promise;
+			},
+
+			getRequirementTemplateData : function(){
+				console.log("dashboard.js : getRequirementTemplateData");
+
+				var promise = $.ajax({
+					async: true,
+					url: "dashboard/getRequirementTemplateData.htm",
+					type: "GET",
+					datatype: "json",
+				}).done(function(result) {
+					result = JSON.parse(result);
+					console.log("\tresult : " + result);
+					if (result.sessionMessage != null && result.sessionMessage != "") {
+						console.log("\t" + result.sessionMessage);
+					} else if (result != null && result.errorMessage != null) {
+						if (result.errorCode == 1000) {
+							console.log("\t" + result.errorMessage);
+						}
+						console.log("\tfailure : " + result.errorMessage);
+					} else {
+						console.log("getRequirementTemplateData : \n\tresult : " + result);
+					}
+				}).fail(function(jqXHR, textStatus) {
+					console.log("getRequirementTemplateData : Application Exception Occured \n\tjqXHR : " + JSON.stringify(jqXHR) + "\n\ttextStatus : " + textStatus);
 				});
 				return promise;
 			},
