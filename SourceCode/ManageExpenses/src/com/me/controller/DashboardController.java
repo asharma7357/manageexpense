b0dc96c6766d0468850201d606a96b603f3ee2a3
Rahil Khan <RahilKhan@users.noreply.github.com>
2017-01-14 10:24:45 +0530

    updated DashboarController.java

--- SourceCode/ManageExpenses/src/com/me/controller/DashboardController.java
+++ SourceCode/ManageExpenses/src/com/me/controller/DashboardController.java
@@ -1,20 +1,30 @@
 package com.me.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+
 
 import com.me.service.inf.DashboardServiceInf;
 
@@ -31,13 +41,26 @@ public class DashboardController {
 	 * This service returns Dashboard data details
 	 * @return String
 	 */
+//	@RequestMapping(value = "/getRequirementTemplateData", method = RequestMethod.GET)
 	@RequestMapping(value = "/getDashboardDetails", method = RequestMethod.GET)
-	public @ResponseBody String getDashBoardDetails(){
+	public Map<String,Object> getDashBoardDetails(){
+		
 		String userId ="rahil";
 		System.out.println("DashboardController");
 		List dashboardDetailsList = dashboardServiceInf.getDashBoardDetails(userId);
-		return "hello";
+		Map<String,Object> dashBoardMap = new HashMap<String, Object>();
+		dashBoardMap.put("dashboard", dashboardDetailsList);
+		System.out.println("DashboardController : \n\tdashboardDetailsList.size() : " + dashboardDetailsList.size() + "\n\tdashboardDetailsList : " + dashboardDetailsList.get(0).toString());
+		
+		return dashBoardMap;
 	}
+/*
+	@RequestMapping(value="/getRequirementTemplateData" , method = RequestMethod.GET)
+	public  Map<String, Object> getRequirementTemplateData(){
+		Map<String,Object> result = headerService.getRequirementTemplateData();
+		return result;
 
+	}
+*/	
 	
 }
